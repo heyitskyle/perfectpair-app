4c40b475d0698593ba57c3761ba3a4e86c818619
HeyItsKyle <inbox@kylelehmann.me>
2024-04-04 20:15:38 -0500

    initial setup

---
+++ The Perfect PairUITests/The_Perfect_PairUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  The_Perfect_PairUITestsLaunchTests.swift
+//  The Perfect PairUITests
+//
+//  Created by Kyle Lehmann on 4/4/24.
+//
+
+import XCTest
+
+final class The_Perfect_PairUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
