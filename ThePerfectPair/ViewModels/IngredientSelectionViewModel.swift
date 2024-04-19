a6397a38d3786f87eca4f231e27931e72dfd4433
HeyItsKyle <inbox@kylelehmann.me>
2024-04-18 20:58:52 -0500

    push to GitHub for capstone progress. working on ViewModels. Data models need more testing. Views uncreated.

---
+++ ThePerfectPair/ViewModels/IngredientSelectionViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  IngredientSelectionViewModel.swift
+//  ThePerfectPair
+//
+//  Created by Kyle Lehmann on 4/18/24.
+//
+
+import Foundation
+import SwiftData
--- ThePerfectPairTests/IngredientDataServiceTests.swift
+++ ThePerfectPairTests/IngredientDataServiceTests.swift
@@ -6,22 +6,30 @@
 //
 
 import XCTest
+@testable import ThePerfectPair
 
 final class IngredientDataServiceTests: XCTestCase {
+    var ingredientService: IngredientDataService!
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+        ingredientService = IngredientDataService()
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        ingredientService = nil
+        super.tearDown()
     }
 
-    func testExample() throws {
+    func testInit() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         // Any test you write for XCTest can be annotated as throws and async.
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        XCTAssertNotNil(ingredientService, "IngredientDataService not initialized")
     }
 
 }
